ef2f3632913784310b90c4a02022c6083de2b70f
dawn-cmd <hjing7@wisc.edu>
2023-06-11 12:31:54 -0400

    leetcode hot 100

---
+++ 108. 将有序数组转换为二叉搜索树.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if len(nums) == 0:
+            return None
+        root = TreeNode(val=nums[len(nums) // 2])
+        root.left = self.sortedArrayToBST(nums[:len(nums) // 2])
+        root.right = self.sortedArrayToBST(nums[len(nums) // 2 + 1:])
+        return root
