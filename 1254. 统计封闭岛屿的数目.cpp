92d24a709952991f4270bf08852706c9ca12543c
dawn-cmd <hjing7@wisc.edu>
2023-06-17 22:15:36 -0400

    update

--- 1254. 统计封闭岛屿的数目.cpp
+++ 1254. 统计封闭岛屿的数目.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 class Solution {
 public:
-    void bfs(const vector<vector<int>> &grid) {
+    void bfs(const vector<vector<int>> &grid, vector<vector<int>> &vis, const int &x, const int &y) {
         
     }
     int closedIsland(vector<vector<int>> &grid) {
