e56688cfbce73e3c3867d1173ca1ab2ffb589374
dawn-cmd <hjing7@wisc.edu>
2023-06-28 11:54:55 -0400

    update

---
+++ 51_N 皇后.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        
+    }
+};
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+}
+
