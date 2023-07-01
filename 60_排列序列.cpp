ba38c8b5d9e8137782ee8b8b30e200f5977c0c62
dawn-cmd <hjing7@wisc.edu>
2023-07-01 12:02:30 -0400

    updatre

--- 60_排列序列.cpp
+++ 60_排列序列.cpp
@@ -1,29 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-private:
-    int cnt;
-    string ans;
-    void dfs(vector<int> &h, int step, int k, string cur) {
-        if (step == h.size() - 1) {
-            ++cnt;
-            if (cnt == k) ans = cur;
-            return;
-        }
-        if (cnt >= k) return;
-        for (int i = 1; i < h.size(); ++i) {
-            if (h[i]) continue;
-            h[i] = 1;
-            dfs(h, step + 1, k, cur + (char)('0' + i));
-            h[i] = 0;
-        }
-    }
-
 public:
     string getPermutation(int n, int k) {
-        vector<int> h(n + 1, 0);
-        dfs(h, 0, k, "");
-        return ans;
+        vector<int> factorial(n);
+        factorial[0] = 1;
+        for (int i = 1; i < n; ++i) factorial[i] = factorial[i - 1] * i;
+        string ans = "";
+        k--;
+        for (int i = 1; i <= n; ++i) {
+            int order = k / factorial[n - i] + 1;
+            
+        }
     }
 };
 int main() {
