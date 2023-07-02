b7910a8d740b3bc9f9ff9a8504bc1387b6cd8c92
dawn-cmd <hjing7@wisc.edu>
2023-07-02 09:38:21 -0400

    update

---
+++ 65_有效数字.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isNumber(string s) {
+        for (int i = 0; i < s.size(); ++i) {
+            if (!isdigit(s[i]) && s[i] != e)
+        }
+    }
+};
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+}
