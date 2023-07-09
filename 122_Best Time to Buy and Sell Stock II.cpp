1915b6ccd6fbcb2eadb1d94213ea1d83a7dced41
dawn-cmd <hjing7@wisc.edu>
2023-07-08 20:58:35 -0400

    daliy problem

---
+++ 122_Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(), vector<int>(2, 0));
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < dp.size(); ++i) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[prices.size() - 1][0];
+    }
+};
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+}
+
