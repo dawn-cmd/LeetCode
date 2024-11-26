43fc04a60b8b757d5e0f2a415731f59b25895bb0
Huaiyuan Jing <ls.hyjing@gmail.com>
2024-11-26 11:58:38 -0600

    update project

---
+++ src/bin/p3208交替组 II.rs
@@ -0,0 +1,8 @@
+struct Solution;
+impl Solution {
+    pub fn number_of_alternating_groups(colors: Vec<i32>, k: i32) -> i32 {
+        let mut colors = colors;
+        colors.extend(colors);
+        
+    }
+}
